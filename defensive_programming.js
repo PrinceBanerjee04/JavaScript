1fb6b1f3b88aeb46c9e3033dd985441717d279f2
PrinceBanerjee04 <130071684+PrinceBanerjee04@users.noreply.github.com>
2023-12-28 12:17:03 +0530

    Create defensive_programming.js

---
+++ defensive_programming.js
@@ -0,0 +1,19 @@
+function letterFinder(word, match) {
+    var condition1 = typeof(word) == 'string' && word.length >= 2; //if the word is a string and the length is greater than or equal to 2
+    var condition2 = typeof(match) == 'string' && match.length == 1; //if the match is a string and the length is equal to 1
+    if(condition1 && condition2) { //if both condition matches
+        for(var i = 0; i < word.length; i++) {
+            if(word[i] == match) {
+                //check if the character at this i position in the word is equal to the match
+                console.log('Found the', match, 'at', i)
+            } else {
+                console.log('---No match found at', i)
+            }
+        }
+    } else {
+        //if the requirements don't match
+        console.log("Please pass correct arguments to the function")
+    }
+}
+letterFinder([],[])
+letterFinder("cat","c")
