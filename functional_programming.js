e858d40c36a30a9629835fd7bb70a4d11301e4cc
PrinceBanerjee04 <130071684+PrinceBanerjee04@users.noreply.github.com>
2023-12-29 13:22:19 +0530

    Create functional_programming.js

---
+++ functional_programming.js
@@ -0,0 +1,14 @@
+
+
+var currencyOne=100;
+var currencyTwo=0;
+var exchangeRate=1.2;
+
+function convertCurrency(amount,rate)
+{
+    return amount*rate;
+}
+
+currencyTwo=convertCurrency(currencyOne,exchangeRate);
+
+console.log(currencyTwo);
