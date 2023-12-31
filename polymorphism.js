d0b1a02a4b632cd10882a199c04a4a3e1eeab471
PrinceBanerjee04 <130071684+PrinceBanerjee04@users.noreply.github.com>
2023-12-31 11:23:52 +0530

    Create polymorphism.js

---
+++ polymorphism.js
@@ -0,0 +1,20 @@
+class Bird {
+    useWings() {
+        console.log("Flying!")
+    }
+}
+class Eagle extends Bird {
+    useWings() {
+        super.useWings()
+        console.log("Barely flapping!")
+    }
+}
+class Penguin extends Bird {
+    useWings() {
+        console.log("Diving!")
+    }
+}
+var baldEagle = new Eagle();
+var kingPenguin = new Penguin();
+baldEagle.useWings(); // "Flying! Barely flapping!"
+kingPenguin.useWings(); // "Diving!"
